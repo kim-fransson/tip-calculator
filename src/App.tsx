b22bb3eaab4d2d5892741c42a7fe26ba4d10320b
Kim Fransson <kim.fransson.22@gmail.com>
2025-05-09 14:03:20 +0200

    implemented Results

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { I18nProvider } from "react-aria-components";
-import "./App.css";
-import { Currency } from "./components/Currency/Currency";
 function App() {
-  return (
-    <I18nProvider locale="en-US">
-      <Currency value={0} />
-    </I18nProvider>
-  );
+  return <I18nProvider locale="en-US">...</I18nProvider>;
 }
 
 export default App;
---
+++ src/components/Results.tsx
@@ -0,0 +1,74 @@
+import { Button, type ButtonProps } from "react-aria-components";
+import { Currency } from "./Currency";
+import { twJoin } from "tailwind-merge";
+
+interface ResultsProps {
+  tipAmountPerPerson?: number;
+  totalAmountPerPerson?: number;
+  isResetDisabled?: boolean;
+  onReset?: () => void;
+}
+export const Results = ({
+  tipAmountPerPerson,
+  totalAmountPerPerson,
+  isResetDisabled = true,
+  onReset = () => {},
+}: ResultsProps) => {
+  return (
+    <ResultsContent>
+      <ResultsDetails>
+        <ResultsRow>
+          <ResultsLabel label="Tip Amount" />
+          <Currency value={tipAmountPerPerson || 0} />
+        </ResultsRow>
+
+        <ResultsRow>
+          <ResultsLabel label="Total" />
+          <Currency value={totalAmountPerPerson || 0} />
+        </ResultsRow>
+      </ResultsDetails>
+      <ResetButton isDisabled={isResetDisabled} onPress={onReset} />
+    </ResultsContent>
+  );
+};
+
+const ResultsContent = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="p-6 bg-green-900 grid gap-8 rounded-2xl">{children}</div>
+  );
+};
+
+const ResultsDetails = ({ children }: { children: React.ReactNode }) => {
+  return <div className="grid gap-6">{children}</div>;
+};
+
+const ResultsRow = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex flex-wrap justify-between">{children}</div>;
+};
+
+const ResultsLabel = ({ label }: { label: string }) => {
+  return (
+    <div className="grid">
+      <span className="text-white font-bold">{label}</span>
+      <span className="text-grey-400 font-bold text-sm">/ person</span>
+    </div>
+  );
+};
+
+const ResetButton = (props: ButtonProps) => {
+  return (
+    <Button
+      {...props}
+      className={twJoin(
+        "px-8 py-2 h-12 text-xl tracking-wider rounded-sm font-bold outline-0 transition-all duration-200",
+        "text-green-900 bg-green-400 cursor-pointer",
+        "hover:bg-green-200",
+        "pressed:scale-95",
+        "focus-visible:outline-2 focus-visible:outline-blue-400 focus-visible:outline-offset-2",
+        "disabled:text-green-800 disabled:bg-green-750 disabled:cursor-not-allowed"
+      )}
+    >
+      RESET
+    </Button>
+  );
+};
